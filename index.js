d2b80da484152f61a055b601b9533b02870b2281
Ayana Bando <ayanaflax@yahoo.com>
2019-01-15 15:24:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
-    })
+    });
 }
-
-preventRefreshOnSubmit()
+function retrieveEmployeeInformation(){
+ return $(''); 
+}
+preventRefreshOnSubmit();
